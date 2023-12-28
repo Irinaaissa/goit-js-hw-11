c12eb7d24dba498a7d4b152a6d3bd92f82416a7e
Irinaaissa <irinaalisa7506@gmail.com>
2023-12-28 17:45:40 +0200

    2

--- src/main.js
+++ src/main.js
@@ -4,41 +4,49 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const gallery = document.getElementById('gallery');
 
-
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '41471340-7b105b7a368edb6edd340273b';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 searchForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     const query = searchInput.value;
     try {
-        const response = await fetch(`${getBaseUrl()}&q=${query}&safesearch=true`);
+        const response = await fetch(`${getBaseUrl()}&q=${query}`);
         const data = await response.json();
         const images = data.hits;
-
         if (images.length > 0) {
             gallery.innerHTML = '';
-
-            images.forEach(image => {
-                const imgElement = document.createElement('img')
-                imgElement.src = image.webformatURL;
-                gallery.appendChild(imgElement);
-            });
+            gallery.innerHTML = images.reduce((html, { BASE_URL, images, }) => html + `
+<li class="gallery-item">
+  <a class="gallery-link" href="${images}" download="none">
+    <img
+      class="gallery-image"
+      src="${BASE_URL}"
+      data-source="${images}"
+      
+    />
+  </a>
+</li>`, '');
+            lightbox.refresh();
         } else {
             iziToast.error({
                 title: 'Error',
-                message: 'Sorry, there are no images matching yo'
+                message: 'Sorry, there are no images matching your search'
             });
         }
     } catch (error) {
         console.error(error);
     }
-    
 });
 
 const getBaseUrl = () => {
@@ -47,12 +55,3 @@ const getBaseUrl = () => {
 
     return url;
 };
-
-
-
-new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionsData: 'alt',
-  captionDelay: 250,
-  
-});
