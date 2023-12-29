b2a4be1a455a8973895084398b8aec50758d87ce
Irinaaissa <irinaalisa7506@gmail.com>
2023-12-29 23:51:19 +0200

    23

--- src/main.js
+++ src/main.js
@@ -16,81 +16,86 @@ const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
 });
-const loader = document.querySelector(".loader");
+const loader = document.querySelector('.loader');
 
 function showLoadingIndicator() {
-    loader.style.display = "block";
-  }
+    loader.style.display = 'block';
+}
 
-  function hideLoadingIndicator() {
-    loader.style.display = "none";
-  }
+function hideLoadingIndicator() {
+    loader.style.display = 'none';
+}
 
-searchForm.addEventListener('submit', async (event) => {
+searchForm.addEventListener('submit', async event => {
     event.preventDefault();
+
     const query = searchInput.value;
-     showLoadingIndicator();
+
+    if (query.trim() === '') {
+        iziToast.error({
+            title: 'Error',
+            message: 'Sorry, there are no images matching your search',
+        });
+
+        return hideLoadingIndicator();
+    }
+
+    showLoadingIndicator();
+
     try {
-        const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${query}`);
-        const {hits,totalHits} = await response.json();
-        if (query ===  "") {
-            iziToast.error({
-                title: 'Error',
-                message: 'Sorry, there are no images matching your search'
-                
-            });
-            return hideLoadingIndicator();
-            
+        const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}`);
+
+        if (!response.ok) {
+            throw new Error(response.status);
         }
+
+        const { hits } = await response.json();
+
         gallery.innerHTML = '';
-        const markup = hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-        
-            <li class="gallery-item">
-                <a class="gallery-link" href="${largeImageURL}" download="none">
-                    <img
-                        class="gallery-image"
-                        src="${webformatURL}"
-                        data-source="${largeImageURL}"
-                        alt="${tags}"
-                    />
-                </a>
-                
-                <div class="card-info">
-                <div class="field">
-                    <span class="label">Likes</span>
-                    <span class="value">${likes}</span>    
-                </div>
-                <div class="field">
-                    <span class="label">Views</span>
-                    <span class="value">${views}</span>   
-                </div>
-                <div class="field">
-                    <span class="label">Comments</span>
-                    <span class="value">${comments}</span>    
-                </div>
-                <div class="field">
-                    <span class="label">Downloads</span>
-                    <span class="value">${downloads}</span>    
-                </div>
-            </div>
-            </li>
-        `);
-        gallery.insertAdjacentHTML("beforeend",markup);
+
+        const markup = hits
+            .map(
+                ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}" download="none">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              data-source="${largeImageURL}"
+              alt="${tags}"
+            />
+          </a>
+          <div class="card-info">
+          <div class="field">
+              <span class="label">Likes</span>
+              <span class="value">${likes}</span>    
+          </div>
+          
+          <div class="field">
+              <span class="label">Views</span>
+              <span class="value">${views}</span>   
+          </div>
+          <div class="field">
+              <span class="label">Comments</span>
+              <span class="value">${comments}</span>    
+          </div>
+          <div class="field">
+              <span class="label">Downloads</span>
+              <span class="value">${downloads}</span>    
+          </div>
+          </div>
+        </li>
+      `
+            )
+            .join('');
+
+        gallery.insertAdjacentHTML('beforeend', markup);
+
         lightbox.refresh();
     } catch (error) {
-        
-
-
-        console.error(error); 
+        console.error(error);
+    } finally {
+        hideLoadingIndicator();
+        event.target.reset();
     }
-     hideLoadingIndicator();
-
-    event.target.reset();
 });
-
-const getBaseUrl = () => {
-    const url = new URL(BASE_URL);
-    url.searchParams.append('key', API_KEY);
-
-    return url;
-};
