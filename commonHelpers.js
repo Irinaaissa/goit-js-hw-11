5e40721965f4be04331771154b0011aa5c0a23d8
Irinaaissa <140831632+Irinaaissa@users.noreply.github.com>
2024-01-02 12:46:58 +0000

    Deploying to gh-pages from @ Irinaaissa/goit-js-hw-11@bb63f3e649ba244843fa7f609eb49cff1e71c8b7 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,32 +1,32 @@
-import{S as y,i as g}from"./assets/vendor-9310f15c.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function t(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(e){if(e.ep)return;e.ep=!0;const s=t(e);fetch(e.href,s)}})();const h=document.getElementById("search-form"),v=document.getElementById("search-input"),l=document.getElementById("gallery"),b="https://pixabay.com/api/",L="41471340-7b105b7a368edb6edd340273b",$=new y(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250}),c=document.querySelector(".loader");function w(){c.style.display="block"}function i(){c.style.display="none"}h.addEventListener("submit",async o=>{o.preventDefault();const a=v.value;if(a.trim()==="")return g.error({title:"Error",message:"Sorry, there are no images matching your search"}),i();w();try{const t=await fetch(`${b}?key=${L}&q=${a}`);if(!t.ok)throw new Error(t.status);const{hits:n}=await t.json();l.innerHTML="";const e=n.map(({webformatURL:s,largeImageURL:r,tags:d,likes:u,views:p,comments:f,downloads:m})=>`
+import{S as g,i as l}from"./assets/vendor-9310f15c.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const a of s.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(e){if(e.ep)return;e.ep=!0;const s=r(e);fetch(e.href,s)}})();const h=document.getElementById("search-form"),v=document.getElementById("search-input"),i=document.getElementById("gallery"),b="https://pixabay.com/api/",L="41471340-7b105b7a368edb6edd340273b",E=new g(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250}),d=document.querySelector(".loader");function $(){d.style.display="block"}function c(){d.style.display="none"}h.addEventListener("submit",async o=>{o.preventDefault();const t=v.value;if(t.trim()==="")return l.error({title:"Error",message:"Sorry, there are no images matching your search"}),c();$();try{const r=await fetch(`${b}?key=${L}&q=${t}`);if(!r.ok)throw new Error(r.status);const{hits:n}=await r.json();i.innerHTML="";const e=n.map(({webformatURL:s,largeImageURL:a,tags:u,likes:p,views:f,comments:m,downloads:y})=>`
         <li class="gallery-item">
-          <a class="gallery-link" href="${r}" >
+          <a class="gallery-link" href="${a}" >
             <img
               class="gallery-image"
               src="${s}"
-              data-source="${r}"
-              alt="${d}"
+              data-source="${a}"
+              alt="${u}"
             />
           </a>
           <div class="card-info">
           <div class="field">
               <span class="label">Likes</span>
-              <span class="value">${u}</span>    
+              <span class="value">${p}</span>    
           </div>
           
           <div class="field">
               <span class="label">Views</span>
-              <span class="value">${p}</span>   
+              <span class="value">${f}</span>   
           </div>
           <div class="field">
               <span class="label">Comments</span>
-              <span class="value">${f}</span>    
+              <span class="value">${m}</span>    
           </div>
           <div class="field">
               <span class="label">Downloads</span>
-              <span class="value">${m}</span>    
+              <span class="value">${y}</span>    
           </div>
           </div>
         </li>
-      `).join("");l.insertAdjacentHTML("beforeend",e),$.refresh()}catch(t){console.error(t)}finally{i(),o.target.reset()}});
+      `).join("");i.insertAdjacentHTML("beforeend",e),E.refresh()}catch{l.error({title:"Error",message:"Error"})}finally{c(),o.target.reset()}});
 //# sourceMappingURL=commonHelpers.js.map
